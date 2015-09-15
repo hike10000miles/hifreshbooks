98cc5a390a8e1850fe111e98f6a025962c4e9275
hike10000miles <chenshen4009@gmail.com>
2015-09-15 16:29:06 -0400

    change background and so on

---
+++ app/scripts/main.js
@@ -0,0 +1,37 @@
+'use strict';
+
+(function($) {
+  $(document).ready(function() {
+
+    function toggleButton() {
+      $('#sideBar').find('.button').toggle(400);
+
+    }
+    
+    function sendAJAX1() {
+      $('#view').load('page1.html', function() {
+        $('#view').find('div').fadeIn('slow');
+      });
+    }
+    
+    function sendAJAX2() {
+      $('#view').load('page2.html', function() {
+        $('#view').find('div').fadeIn('slow');
+      });
+    }
+    
+    function sendAJAX3() {
+      $('#view').load('page3.html', function() {
+        $('#view').find('div').fadeIn('slow');
+      });
+    }
+    
+    $('#load1').click(sendAJAX1);
+    $('#load2').click(sendAJAX2);
+    $('#load3').click(sendAJAX3);
+    $('#menu').click(toggleButton);
+
+
+  
+  });
+})(jQuery);
