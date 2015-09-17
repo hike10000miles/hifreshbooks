5d52737fe720d9c27608a33fd8b9e19f5ce2a309
hike10000miles <chenshen4009@gmail.com>
2015-09-17 10:11:58 -0400

    Add basic contain.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,18 +11,24 @@
     function sendAJAX1() {
       $('#view').load('page1.html', function() {
         $('#view').find('div').fadeIn('slow');
+        $('#header').find('h1').hide();
+        $('#header').find('h2').text('My name is Chen Shen.');
       });
     }
     
     function sendAJAX2() {
       $('#view').load('page2.html', function() {
         $('#view').find('div').fadeIn('slow');
+        $('#header').find('h1').hide();
+        $('#header').find('h2').text('My name is Chen Shen.');
       });
     }
     
     function sendAJAX3() {
       $('#view').load('page3.html', function() {
         $('#view').find('div').fadeIn('slow');
+        $('#header').find('h1').hide();
+        $('#header').find('h2').text('My name is Chen Shen.');
       });
     }
     
