85f06160c6065ddacac2b8f9b0515aa47cf81b02
hike10000miles <chenshen4009@gmail.com>
2015-09-22 16:37:44 -0400

    need some prove reading

--- app/scripts/pagecontrol.js
+++ app/scripts/pagecontrol.js
@@ -13,16 +13,9 @@
         $('#view').find('div').fadeIn('slow');
       });
     }
-    
-    function sendAJAX3() {
-      $('#view').load('page3.html', function() {
-        $('#view').find('div').fadeIn('slow');
-      });
-    }
-
+  
     $('#load1').click(sendAJAX1);
     $('#load2').click(sendAJAX2);
-    $('#load3').click(sendAJAX3);
 
   });
 })(jQuery);
