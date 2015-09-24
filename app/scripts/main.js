d3a12c2c60a657170ca0fce1587ea82e70af7d73
hike10000miles <chenshen4009@gmail.com>
2015-09-24 13:39:10 -0400

    new resume page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -23,10 +23,19 @@
         $('#header').find('h2').text('My name is Chen Shen.');
       });
     }
+
+    function sendAJAX3() {
+      $('#view').load('page3.html', function() {
+        $('#view').find('div').fadeIn('slow');
+        $('#header').find('h1').hide();
+        $('#header').find('h2').text('My name is Chen Shen.');
+      });
+    }
     
     
     $('#load1').click(sendAJAX1);
     $('#load2').click(sendAJAX2);
+    $('#load3').click(sendAJAX3);    
     $('#menu').click(toggleButton);
 
 
